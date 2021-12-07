82813636dbadb10e2c0b6872348f438c5912f76d
sdxsd <willmaguire@icloud.com>
2021-12-07 15:51:50 +0100

    Added license header to source file

--- src/main.c
+++ src/main.c
@@ -1,3 +1,21 @@
+/*
+	GNL_TESTER (tester for 42 project get_next_line)
+   	Copyright (C) 2021  Will Maguire
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 #include "../include/gnl_tester.h"
 #include "../../get_next_line/get_next_line.h"
 
